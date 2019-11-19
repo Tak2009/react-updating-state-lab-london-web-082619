b2baa142f5f497582f1b4b6324ccf7393b193cca
github username <github email address>
2019-11-19 00:54:50 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,38 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+    state = {
+        timesClicked: 0
+    }
+    
+    // //check with Morgan
+    // handleClick = () => {
+    //     let newTimesClicked = this.state.timesClicked + 1
+    //     this.setState ({
+    //         timesClicked: newTimesClicked
+              
+    //         })
+    // }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            console.log(previousState)
+            console.log(previousState.timesClicked)
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+    
+
+    render () {
+        return (
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,51 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+        bitrate: 8,
+        video: {
+           resolution: '1080p'
+           }
+        }
+    }
+
+    handleBit = () => {
+        this.setState({
+                settings: {
+                ...this.state.settings,
+                bitrate: 12
+                }
+        })
+        // console.log(this.state)
+    }
+
+    handleRes = () => {
+        
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                    video: {
+                        resolution: '720p'
+                    }
+                }
+            })
+    
+        console.log(this.state)
+    }
+
+    render () {
+        return (
+        <div>
+        <button onClick={this.handleBit}>{this.state.settings.bitrate}</button>
+        <button onClick={this.handleRes}>{this.state.settings.video.resolution}</button>
+        </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
